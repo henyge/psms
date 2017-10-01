8e7a85eeb2231a341d2e2d390be7fff984cd7f7c
henyge <henyge@PRICOM-DEV>
2017-10-01 23:21:46 +0000

    Login goes to main page

--- src/schoolnew/FXMLDocumentController.java
+++ src/schoolnew/FXMLDocumentController.java
@@ -5,14 +5,18 @@
  */
 package schoolnew;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.*;
-import javax.swing.JOptionPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 /**
  *
@@ -22,21 +26,32 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private ImageView pTitle;
+    
+    @FXML
+    private Schoolnew sn;
+    
+    @FXML
+    private Button btnLogin;
+    
+    private Stage stage;
 
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        //System.out.println(((Button)event.getSource()).getText());
-        //System.out.println(((Button)event.getSource()).getId());
-        JOptionPane.showMessageDialog(null, ((Button)event.getSource()).getId(), "Button id clicked", JOptionPane.INFORMATION_MESSAGE);
-        //label.setText("Hello World!");
+    private void handleButtonAction(ActionEvent event) throws IOException {
+        String id = ((Button)event.getSource()).getId();
+        
+        if(id.equals("btnLogin")){
+            stage = (Stage) btnLogin.getScene().getWindow();
+            AnchorPane root;
+            root = (AnchorPane) FXMLLoader.load(getClass().getResource("FXMLMainPanel.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
         pTitle.setImage(new Image("/assets/soft_name.png"));
-
-        
     }
 
 }
